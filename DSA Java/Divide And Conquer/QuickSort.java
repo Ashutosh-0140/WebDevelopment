e10afb6973d91ad95d9bb534d672c2b45f48dfa9
Ashutosh-0140 <ashutosh0140@gmail.com>
2026-02-09 20:08:58 +0530

    Learn about Quick sort

---
+++ DSA Java/Divide And Conquer/QuickSort.java
@@ -0,0 +1,44 @@
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            // Partition the array
+            int pivotIndex = partition(arr, low, high);
+
+            // Recursively sort left and right halves
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high]; // choose last element as pivot
+        int i = low - 1;       // index of smaller element
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // place pivot in correct position
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1; // return pivot index
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {6, 3, 9, 5, 2, 8};
+        quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sorted array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
