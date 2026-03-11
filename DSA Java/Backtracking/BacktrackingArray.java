9dc520ecb195a94062097465f22d17796f89ba96
Ashutosh-0140 <ashutosh0140@gmail.com>
2026-03-11 07:30:03 +0530

    Auto commit at 11-03-2026  7:30:03.11

---
+++ DSA Java/Backtracking/BacktrackingArray.java
@@ -0,0 +1,25 @@
+class BacktrackingArray{
+    public static void changeArray(int arr[], int i, int val){
+        if(i == arr.length){
+            printArray(arr);
+            return; 
+        }
+        arr[i] = val;
+        changeArray(arr, i+1, val+1);
+        arr[i] = arr[i] -2;
+
+
+    } 
+    public static void printArray(int arr[]){
+        for(int n : arr){
+            System.out.print(n+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String s[]){
+        int arr[] = new int[5];
+        changeArray(arr, 0, 1);
+        printArray(arr);
+
+    }
+}
