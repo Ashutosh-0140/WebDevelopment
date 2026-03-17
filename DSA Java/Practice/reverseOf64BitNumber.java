7153fc0103955a051c44fa063500cb526ab7af8f
Ashutosh-0140 <ashutosh0140@gmail.com>
2026-03-17 22:46:51 +0530

    Reverse of a number of 32 bit

---
+++ DSA Java/Practice/reverseOf64BitNumber.java
@@ -0,0 +1,26 @@
+class reverseOf64BitNumber{
+    public static int reverse(int x) {
+        int num = 0;
+        while(x != 0){
+            int temp = x%10;
+
+            if(num > Integer.MAX_VALUE/10 ||(num == Integer.MAX_VALUE/ 10 && temp >7) ){
+                return 0;
+            }
+
+            if(num < Integer.MIN_VALUE/10 || (num == Integer.MIN_VALUE/10 && temp <-8)){
+                return 0;
+            }
+
+            num = num*10 + temp;
+            x /= 10;
+        }
+        return num;
+    }
+
+    public static void main(String s[]){
+        int x = 123;
+        int reversed =reverse(x);
+        System.out.println("Reverse of "+x+" is "+reversed);
+    }
+}
