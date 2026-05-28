742de977637edbfd4526486d079c97456ed03d0d
Ashutosh-0140 <ashutosh0140@gmail.com>
2026-05-28 07:50:25 +0530

    Auto commit at 28-05-2026  7:50:25.37

---
+++ DSA Java/Linked List/DoubleLL.java
@@ -0,0 +1,94 @@
+public class DoubleLL{
+    class Node{
+        int data;
+        Node next;
+        Node prev;
+        Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    public Node head;
+    public Node tail;
+    public int size;
+    //add first
+    public void addFirst(int data){
+        Node newNode = new Node(data);
+        size++;
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+    //add last
+    public void addLast(int data){
+    Node newNode = new Node(data);
+    size++;
+    if(head == null){
+        head = tail = newNode;
+        return;
+    }
+    tail.next = newNode;
+    newNode.prev = tail;
+    tail = newNode;
+}
+
+    //addMiddle
+    public void removeFirst(){
+        if(head == null){
+            System.out.println("No data;");
+        }
+        if(head.next == null){
+            head = tail = null;
+        }
+        head = head.next;
+        head.prev = null;
+        size--;
+        return;
+    }
+    //add last
+    public void removeLast(){
+        if(head == null){
+            System.out.println("No data;");
+        }
+        if(head.next == null){
+            head = tail = null;
+        }
+        tail = tail.prev;
+        tail.next = null;
+        size--;
+        return;
+    }
+    //print
+    public void print(){
+        Node temp = head;
+        if(head == null){
+            System.out.println("Empty list.");
+        }
+        while(temp != null){
+            System.out.print(temp.data+"->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String s[]){
+        DoubleLL dll = new DoubleLL();
+        dll.addFirst(1);
+        dll.print();
+        dll.addLast(2);
+        dll.print();
+        dll.addLast(3);
+        dll.print();
+        dll.addLast(4);
+        dll.print();
+        dll.removeFirst();
+        dll.print();
+        dll.removeLast();
+        dll.print();
+        //System.out.println(+size);
+    }
+}
